12770cddc45d0c1868a4ad81f80028ce3e5fb5fc
hoehlrich <89951187+hoehlrich@users.noreply.github.com>
2022-04-26 07:05:09 -0400

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ class Cubestring():
 
         corners = {
             'c1': [('U', 1), ('L', 3), ('B', 7)],
-            'c2': [('U', 3), ('R', 4), ('B', 9)],
+            'c2': [('U', 3), ('R', 1), ('B', 9)],
             'c3': [('U', 7), ('L', 9), ('F', 1)],
             'c4': [('U', 9), ('R', 7), ('F', 3)],
             'c5': [('D', 3), ('L', 1), ('B', 1)],
@@ -40,7 +40,7 @@ class Cubestring():
             'e4': [('U', 8), ('F', 2)],
             'e5': [('B', 4), ('L', 2)],
             'e6': [('B', 6), ('R', 2)],
-            'e7': [('F', 4), ('L', 6)],
+            'e7': [('F', 4), ('L', 8)],
             'e8': [('F', 6), ('R', 8)],
             'e9': [('D', 2), ('B', 2)],
             'e10': [('D', 4), ('R', 6)],
@@ -48,12 +48,65 @@ class Cubestring():
             'e12': [('D', 8), ('F', 8)]
         }
 
-        cubestring = ''.join(f'____{face}_____' for face in faces)
+        faces = {
+            'U': 0,
+            'R': 1,
+            'F': 2,
+            'D': 3,
+            'L': 4,
+            'B': 5,
+        }
 
-        for corner in corners:
-            pass
-        
-        print(cubestring)
+        # Repeat until a valid result
+        while True:
+            try:
+                cubestring = list(''.join(f'____{face}____' for face in faces))
+
+
+                remaining_corners = list(corners.keys())
+                random.shuffle(remaining_corners)
+
+                # Assign corners to cubestring at random
+                for corner_pos in corners:
+                    # Get chosen corner info and isolate sticker colors
+                    chosen_corner = corners[remaining_corners.pop()]
+                    corner_colors = [sticker[0] for sticker in chosen_corner]
+
+                    # Change permutation and orientation
+                    random.shuffle(corner_colors)
+
+                    # Assign the stickers
+                    for i, sticker in enumerate(corners[corner_pos]):
+                        face, num = sticker
+                        cubestring[faces[face] * 9 + num - 1] = corner_colors[i]
+
+                remaining_edges = list(edges.keys())
+                random.shuffle(remaining_edges)
+
+                # Assign edges to cubestring at random
+                for i, edge_pos in enumerate(edges):
+                    # Get chosen edge info and isolate sticker colors
+                    chosen_edge = edges[remaining_edges.pop()]
+                    edge_colors = [sticker[0] for sticker in chosen_edge]
+
+                    # Change permutation and orientation
+                    random.shuffle(edge_colors)
+
+                    # Assign the stickers
+                    for i, sticker in enumerate(edges[edge_pos]):
+                        face, num = sticker
+
+                        cubestring[faces[face] * 9 + num - 1] = edge_colors[i]
+
+                str_cubestring = ''.join(sticker for sticker in cubestring)
+                
+                print(str_cubestring)
+                solve = kociemba.solve(str_cubestring)
+                print(solve)
+
+                break
+            except ValueError:
+                pass
 
 class Log():
     '''Log Class'''
