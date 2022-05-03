c16374a74de8c8f5f59c8dd7f88a0d6a6c4220df
hoehlrich <89951187+hoehlrich@users.noreply.github.com>
2022-05-02 21:19:05 -0400

    added solving feature

--- main.py
+++ main.py
@@ -57,8 +57,26 @@ class KeyTracker():
 class Cubestring():
     '''Cubestring Class'''
 
-    def __init__(self, random_valid=False):
-        self.cubestring = ''
+    faces = {
+            'U': 0,
+            'R': 1,
+            'F': 2,
+            'D': 3,
+            'L': 4,
+            'B': 5,
+        }
+    
+    orientation = {
+        'white': 'U',
+        'red': 'R',
+        'green': 'F',
+        'yellow': 'D',
+        'orange': 'L',
+        'blue': 'B'
+    }
+
+    def __init__(self, cubestring='', random_valid=False):
+        self.cubestring = cubestring
 
         if random_valid == True:
             self.generate_random_valid_cubestring()
@@ -92,19 +110,10 @@ class Cubestring():
             'e12': [('D', 8), ('F', 8)]
         }
 
-        faces = {
-            'U': 0,
-            'R': 1,
-            'F': 2,
-            'D': 3,
-            'L': 4,
-            'B': 5,
-        }
-
         # Repeat until a valid result
         while True:
             try:
-                cubestring = list(''.join(f'____{face}____' for face in faces))
+                cubestring = list(''.join(f'____{face}____' for face in Cubestring.faces))
 
 
                 remaining_corners = list(corners.keys())
@@ -229,7 +238,7 @@ class App(tk.Tk):
         btn_tracking = Button(buttons_frame, text='Tracking', bd=0, command=self.init_tracking_screen)
         btn_tracking.grid(row=0, column=3, padx=5, pady=5)
 
-        btn_wip = Button(buttons_frame, text='WIP <3', bd=0)
+        btn_wip = Button(buttons_frame, text='Solver', bd=0, command=self.init_solver_screen)
         btn_wip.grid(row=0, column=4, padx=5, pady=5)
 
         # btn hover effect
@@ -241,6 +250,110 @@ class App(tk.Tk):
 
         # Init TimeLog
         self.log = TimeLog(['num', 'time', 'ao5', 'ao12'], self)
+
+    def init_solver_screen(self):
+        self.init_blank_screen()
+
+        # cube input
+        cube_input = Frame(self.mainframe, width=600, height=450)
+        cube_input.pack(side=TOP)
+
+        cube_input.grid_propagate(False)
+
+
+        # Create faces and blanks
+        blank = Frame(cube_input, width=150, height=150, bg=self.light_grey)
+
+
+        # Row 0
+        blank.grid(column=0, row=0)
+
+        white = Frame(cube_input, width=150, height=150, bg='white')
+        white.grid(column=1, row=0)
+
+        blank.grid(column=2, row=0)
+
+        blank.grid(column=3, row=0)
+
+        # Row 1
+        orange = Frame(cube_input, width=150, height=150, bg='orange')
+        orange.grid(column=0, row=1)
+
+        green = Frame(cube_input, width=150, height=150, bg='green')
+        green.grid(column=1, row=1)
+
+        red = Frame(cube_input, width=150, height=150, bg='red')
+        red.grid(column=2, row=1)
+
+        blue = Frame(cube_input, width=150, height=150, bg='blue')
+        blue.grid(column=3, row=1)
+        
+        # Row 2
+        blank.grid(column=0, row=2)
+        
+        yellow = Frame(cube_input, width=150, height=150, bg='yellow')
+        yellow.grid(column=1, row=2)
+
+        blank.grid(column=2, row=2)
+
+        blank.grid(column=2, row=3)
+
+
+        faces = [white, red, green, yellow, orange, blue]
+        
+        
+        # Create stickers
+        self.active_color = 'green'
+
+        def set_color(e):
+            e.widget.configure(bg=self.active_color, activebackground=self.active_color)
+
+        def set_active_color(e):
+            faces = {
+                'u': 'white',
+                'r': 'red',
+                'f': 'green',
+                'd': 'yellow',
+                'l': 'orange',
+                'b': 'blue'
+            }
+
+            if e.keysym in faces.keys():
+                self.active_color = faces[e.keysym]
+            elif e.keysym == 'g':
+                self.cubestring = gen_cubestring()
+                print(kociemba.solve(cubestring=self.cubestring))
+
+        # Gen stickers
+        stickers = {}
+        for face in faces:
+            stickers[face] = [None for i in range(9)]
+            face.grid_propagate(False)
+            for row in range(3):
+                for column in range(3):
+                    sticker_frame = Frame(face, width=50, height=50, bg='black')
+                    sticker_frame.grid(row=row, column=column)
+                    sticker_frame.pack_propagate(False)
+
+                    sticker = Button(sticker_frame, width=40, height=40, bg=face['background'], activebackground=face['background'], relief=RIDGE, borderwidth=3)
+                    sticker.pack()
+                    sticker.bind('<Button>', set_color)
+
+                    stickers[face][row*3 + column] = sticker
+                    
+        self.bind('<Key>', set_active_color)
+
+        button = Button(cube_input, width=5, height=5, text='', command=lambda: set_color(button), bg='white', activebackground='white')
+        button.grid()
+
+        def gen_cubestring():
+            # Gen cubestring via stickers
+            cubestring = ''
+            for face in stickers.values():
+                for sticker in face:
+                    cubestring += Cubestring.orientation[sticker['background']]
+            
+            return cubestring
 
     def init_tracking_screen(self):
         self.init_blank_screen()
